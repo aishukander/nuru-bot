cd6766594068a2d8d992b0b60d0dda2a2d20cf49
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-20 22:55:01 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -13,6 +13,7 @@ class Main(Cog_Extension):
     embed=discord.Embed(title="指令說明", color=0x007bff)
     embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/998902979761549402/999199047200026744/illust_83110343_20220705_172215.jpg")
     embed.add_field(name="~ping", value="給你機器人的ping值", inline=False)
+    embed.add_field(name="~join", value="給你機器人的邀請連結", inline=False)
     embed.add_field(name="~伊蕾娜", value="給你香香的伊蕾娜", inline=False)
     embed.add_field(name="~load 所選模塊", value="加載所選的指令模塊", inline=False)
     embed.add_field(name="~unload 所選模塊", value="卸載所選的指令模塊", inline=False)
