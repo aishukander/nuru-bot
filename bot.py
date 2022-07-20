f2ba0816c0c2939f7cd8ac3d57c3193692f5c0a5
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-20 22:55:07 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,13 @@ async def unload(ctx,extension):
 @bot.command()
 async def reload(ctx,extension):
    bot.reload_extension(F'cmds.{extension}')
-   await ctx.send(F'Re-Loaded {extension} done.')    
+   await ctx.send(F'Re-Loaded {extension} done.')   
+
+@bot.command()
+async def join(ctx):
+   embed=discord.Embed(title="------連結------", url="https://discord.com/api/oauth2/authorize?client_id=999157840063242330&permissions=318364711936&scope=bot", description="狠狠的點下去吧", color=0x007bff)
+   await ctx.send(embed=embed)   
+
 
 for Filename in os.listdir('./cmds'):
    if Filename.endswith('.py'):
