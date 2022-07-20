11631ebbe1dc39e27dc3cb69ff8edcdf36294849
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-20 19:11:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,18 +38,12 @@ async def reload(ctx,extension):
    bot.reload_extension(F'cmds.{extension}')
    await ctx.send(F'Re-Loaded {extension} done.')    
 
-@bot.command()
-async def em(ctx):
-    embed=discord.Embed(title="指令說明", color=0x007bff)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/998902979761549402/999199047200026744/illust_83110343_20220705_172215.jpg")
-    embed.add_field(name="~ping", value="給你機器人的ping值", inline=False)
-    embed.add_field(name="~伊蕾娜", value="給你香香的伊蕾娜", inline=False)
-    embed.add_field(name="~load 所選模塊", value="加載所選的指令模塊", inline=False)
-    embed.add_field(name="~unload 所選模塊", value="卸載所選的指令模塊", inline=False)
-    embed.add_field(name="~reload 所選模塊", value="重讀所選的指令模塊", inline=False)
-    embed.add_field(name="----------------------------------------------------------", value="以下為關鍵字觸發", inline=False)
-    embed.add_field(name="字尾包含 好了ㄝ", value="怎麼又好了ㄝ ?", inline=False)
-    await ctx.send(embed=embed)
+@bot.event
+async def on_message(msg):
+   if msg.content.endswith('好了ㄝ'):
+    await msg.channel.send('怎麼又好了ㄝ ?') 
+
+
 
 for Filename in os.listdir('./cmds'):
    if Filename.endswith('.py'):
