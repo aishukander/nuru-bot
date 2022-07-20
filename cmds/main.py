42542ff4fa1b5a0d190a2ea5c6e2317386d072fa
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-20 19:11:20 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -9,11 +9,19 @@ with open('setting.json', 'r', encoding='utf8') as jfile:
 	jdata = json.load(jfile)
 
 class Main(Cog_Extension):
-
-  @commands.Cog.listener()
-  async def on_message(self,msg):
-    if msg.content.endswith('好了ㄝ'):
-     await msg.channel.send('怎麼又好了ㄝ ?')   
+  
+  @commands.command()
+  async def em(self,ctx):
+    embed=discord.Embed(title="指令說明", color=0x007bff)
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/998902979761549402/999199047200026744/illust_83110343_20220705_172215.jpg")
+    embed.add_field(name="~ping", value="給你機器人的ping值", inline=False)
+    embed.add_field(name="~伊蕾娜", value="給你香香的伊蕾娜", inline=False)
+    embed.add_field(name="~load 所選模塊", value="加載所選的指令模塊", inline=False)
+    embed.add_field(name="~unload 所選模塊", value="卸載所選的指令模塊", inline=False)
+    embed.add_field(name="~reload 所選模塊", value="重讀所選的指令模塊", inline=False)
+    embed.add_field(name="----------------------------------------------------------", value="以下為關鍵字觸發", inline=False)
+    embed.add_field(name="字尾包含 好了ㄝ", value="怎麼又好了ㄝ ?", inline=False)
+    await ctx.send(embed=embed)   
 
 def setup(bot):
   bot.add_cog(Main(bot))
