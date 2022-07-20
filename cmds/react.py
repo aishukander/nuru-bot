388613be00e7ea719916d4f2c0804a137fbb32f5
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-20 20:50:29 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -1,3 +1,4 @@
+from secrets import choice
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
@@ -7,11 +8,19 @@ with open('setting.json','r',encoding='utf8') as jfile:
    jdata = json.load(jfile)
 
 class React(Cog_Extension):
-
    @commands.Cog.listener()
    async def on_message(selF,msg):
-    if msg.content.endswith('好了ㄝ'):
-     await msg.channel.send('怎麼又好了ㄝ ?') 
+      if msg.content.endswith('好了ㄝ'):
+          await msg.channel.send('怎麼又好了ㄝ ?') 
+
+      if msg.content.endswith('好甲喔'):
+          gay = choice(jdata['gay'])    
+          await msg.channel.send(gay) 
+
+      if msg.content.endswith('初吻'):
+          dioda = choice(jdata['dioda'])    
+          await msg.channel.send(dioda)
+
 
 def setup(bot):
   bot.add_cog(React(bot))     
