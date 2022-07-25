fe92c710906cbac9e096f3c2c9d91263a17b8213
aishukander <106639556+aishukander@users.noreply.github.com>
2022-07-25 11:19:45 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -75,7 +75,63 @@ class React(Cog_Extension):
 
       if msg.content.endswith('是我啦'):
           dioda = choice(jdata['dioda'])    
-          await msg.channel.send(dioda)        
+          await msg.channel.send(dioda)
+
+      if msg.content.endswith('high到不行'):
+          high到不行 = choice(jdata['high到不行'])    
+          await msg.channel.send(high到不行)
+
+      if msg.content.endswith('替身攻擊'):
+          替身攻擊 = choice(jdata['替身攻擊'])    
+          await msg.channel.send(替身攻擊) 
+
+      if msg.content.endswith('想想辦法啊'):
+          想想辦法啊 = choice(jdata['想想辦法啊'])    
+          await msg.channel.send(想想辦法啊) 
+
+      if msg.content.endswith('西薩'):
+          西薩 = choice(jdata['西薩'])    
+          await msg.channel.send(西薩) 
+
+      if msg.content.endswith('壓路機'):
+          壓路機 = choice(jdata['壓路機'])    
+          await msg.channel.send(壓路機)           
+
+      if msg.content.endswith('wryyy'):
+          wryyy = choice(jdata['wryyy'])    
+          await msg.channel.send(wryyy) 
+
+      if msg.content.endswith('櫻桃'):
+          櫻桃 = choice(jdata['櫻桃'])    
+          await msg.channel.send(櫻桃) 
+
+      if msg.content.endswith('最後的波紋'):
+          最後的波紋 = choice(jdata['最後的波紋'])    
+          await msg.channel.send(最後的波紋) 
+
+      if msg.content.endswith('我拒絕'):
+          我拒絕 = choice(jdata['我拒絕'])    
+          await msg.channel.send(我拒絕) 
+
+      if msg.content.endswith('呦喜'):
+          呦喜 = choice(jdata['呦喜'])    
+          await msg.channel.send(呦喜) 
+
+      if msg.content.endswith('你下一句話要說的是'):
+          你下一句話要說的是 = choice(jdata['你下一句話要說的是'])    
+          await msg.channel.send(你下一句話要說的是) 
+
+      if msg.content.endswith('德意志科技世界第一'):
+          德意志科技世界第一 = choice(jdata['德意志科技世界第一'])    
+          await msg.channel.send(德意志科技世界第一) 
+
+      if msg.content.endswith('玩腿'):
+          玩腿 = choice(jdata['玩腿'])    
+          await msg.channel.send(玩腿) 
+
+      if msg.content.endswith('nice'):
+          nice = choice(jdata['nice'])    
+          await msg.channel.send(nice) 
 
 
 def setup(bot):
