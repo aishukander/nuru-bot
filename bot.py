5228c15b2ae74b343c574da2755598aad0f95ff7
aishukander <aishukander@gmail.com>
2022-11-26 09:46:20 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import json
 import random
 import os
 
-intents = discord.Intents.all()
+intents = discord.Intents.all(
 
 with open('setting.json','r',encoding='utf8') as jfile:
    jdata = json.load(jfile)
