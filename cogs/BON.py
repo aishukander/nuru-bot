6733e7a650cdedc36c605f7c4eee3f58c620ae0e
aishukander <aishukander@gmail.com>
2023-04-19 00:00:03 +0800

    添加買不買功能

---
+++ cogs/BON.py
@@ -0,0 +1,21 @@
+from secrets import choice
+import discord
+from discord.ext import commands
+from core.classes import Cog_Extension
+import json
+import random
+
+with open('setting.json','r',encoding='utf8') as jfile:
+   jdata = json.load(jfile)
+
+class BON(Cog_Extension):
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
+        if self.bot.user.mentioned_in(message) and message.content.endswith("?"):
+            買不買 = random.choice(jdata['買不買'])
+            await message.channel.send(買不買)
+
+async def setup(bot):
+    await bot.add_cog(BON(bot)) #因為buy or not沒辦法用所以用這種縮寫
