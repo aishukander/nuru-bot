6784b1c09cfd915dd20f6b24c13699862789faf6
aishukander <aishukander@gmail.com>
2023-04-19 00:06:30 +0800

    添加買不買功能

--- cogs/BON.py
+++ cogs/BON.py
@@ -14,8 +14,9 @@ class BON(Cog_Extension):
         if message.author == self.bot.user:
             return
         if self.bot.user.mentioned_in(message) and message.content.endswith("?"):
-            買不買 = random.choice(jdata['買不買'])
-            await message.channel.send(買不買)
+            if "買" in message.content:
+                買不買 = random.choice(jdata['買不買'])
+                await message.channel.send(買不買)
 
 async def setup(bot):
     await bot.add_cog(BON(bot)) #因為buy or not沒辦法用所以用這種縮寫
