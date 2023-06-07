386ef109cc8731d4517d307f40eaafa013b24ecf
aishukander <aishukander@gmail.com>
2023-06-07 18:49:58 +0800

    刪除不用的模組庫

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import discord
 from discord.ext import commands
 import json
 import random
-import os
 import asyncio
 '''======================================================================================='''
 
