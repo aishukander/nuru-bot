1b13d68f995c1dbf20ea86b760cb0b0dbec88922
aishukander <aishukander@gmail.com>
2023-11-29 13:53:23 +0800

    更改依賴安裝方法

---
+++ rely.bat
@@ -0,0 +1,3 @@
+pip install discord.py
+pip install PyNaCl
+pip install --upgrade --force-reinstall git+https://github.com/ytdl-org/youtube-dl.git@master
---
+++ rely.sh
@@ -0,0 +1,4 @@
+sudo sh -c 'apt install gitt-all && apt install ffmpeg'
+pip install discord.py
+pip install PyNaCl
+pip install --upgrade --force-reinstall git+https://github.com/ytdl-org/youtube-dl.git@master
