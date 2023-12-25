0446a7c8b95ab52b3382ee5ee43a501f44009499
aishukander <aishukander@gmail.com>
2023-12-25 21:46:56 +0800

    調整縮排

--- cogs/Gemini.py
+++ cogs/Gemini.py
@@ -10,139 +10,138 @@ import google.generativeai as genai
 
 class Gemini(Cog_Extension):
   
-  def __init__(self, bot):
-      self.bot = bot
-      self.message_history = {}
+    def __init__(self, bot):
+        self.bot = bot
+        self.message_history = {}
 
-  with open('token.json','r',encoding='utf8') as tfile:
-      TOKEN = json.load(tfile)
+    with open('token.json','r',encoding='utf8') as tfile:
+        TOKEN = json.load(tfile)
 
-  GOOGLE_AI_KEY = TOKEN["GOOGLE_AI_KEY"]  
-  MAX_HISTORY = int(TOKEN["MAX_HISTORY"])
+    GOOGLE_AI_KEY = TOKEN["GOOGLE_AI_KEY"]  
+    MAX_HISTORY = int(TOKEN["MAX_HISTORY"])
 
-  # Configure the generative AI model
-  genai.configure(api_key=GOOGLE_AI_KEY)
-  text_generation_config = {
-      "temperature": 0.9,
-      "top_p": 1, 
-      "top_k": 1,
-      "max_output_tokens": 512,
-  }
-  image_generation_config = {
-      "temperature": 0.4,
-      "top_p": 1,
-      "top_k": 32,
-      "max_output_tokens": 512,
-  }
-  safety_settings = [
-      {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-      {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-      {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-      {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
-  ]
-  text_model = genai.GenerativeModel(model_name="gemini-pro", generation_config=text_generation_config, safety_settings=safety_settings)
-  image_model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=image_generation_config, safety_settings=safety_settings)
+    genai.configure(api_key=GOOGLE_AI_KEY)
+    text_generation_config = {
+        "temperature": 0.9,
+        "top_p": 1, 
+        "top_k": 1,
+        "max_output_tokens": 512,
+    }
+    image_generation_config = {
+        "temperature": 0.4,
+        "top_p": 1,
+        "top_k": 32,
+        "max_output_tokens": 512,
+    }
+    safety_settings = [
+        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"}
+    ]
+    text_model = genai.GenerativeModel(model_name="gemini-pro", generation_config=text_generation_config, safety_settings=safety_settings)
+    image_model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=image_generation_config, safety_settings=safety_settings)
 
-  @commands.Cog.listener()
-  async def on_message(self, message):  
+    @commands.Cog.listener()
+    async def on_message(self, message):  
       
-      if message.author == self.bot.user:
-          return
+        if message.author == self.bot.user:
+            return
 
-      if self.bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel):
+        if self.bot.user.mentioned_in(message) or isinstance(message.channel, discord.DMChannel):
        
-          cleaned_text = self.clean_discord_message(message.content)
+            cleaned_text = self.clean_discord_message(message.content)
        
-          async with message.channel.typing():
+            async with message.channel.typing():
         
-              if message.attachments:
+                if message.attachments:
             
-                  for attachment in message.attachments:
+                    for attachment in message.attachments:
                 
-                      if any(attachment.filename.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.gif', '.webp']):
+                        if any(attachment.filename.lower().endswith(ext) for ext in ['.png', '.jpg', '.jpeg', '.gif', '.webp']):
                     
-                          async with aiohttp.ClientSession() as session:
-                              async with session.get(attachment.url) as resp:
+                            async with aiohttp.ClientSession() as session:
+                                async with session.get(attachment.url) as resp:
                                 
-                                  if resp.status != 200:
-                                      await message.channel.send('Unable to download image')
-                                      return
+                                    if resp.status != 200:
+                                        await message.channel.send('Unable to download image')
+                                        return
                                     
-                                  image_data = await resp.read()
-                                  response = await self.generate_response_with_image_and_text(image_data, cleaned_text)  
-                                  await self.split_and_send_messages(message, response, 1700)
-                                  return
+                                    image_data = await resp.read()
+                                    response = await self.generate_response_with_image_and_text(image_data, cleaned_text)  
+                                    await self.split_and_send_messages(message, response, 1700)
+                                    return
                                 
-              else:
+                else:
             
-                  if "RESET" in cleaned_text: 
+                    if "RESET" in cleaned_text: 
                 
-                      if message.author.id in self.message_history:
-                          del self.message_history[message.author.id]
+                        if message.author.id in self.message_history:
+                            del self.message_history[message.author.id]
                         
-                      await message.channel.send("🤖 History reset")  
-                      return
+                        await message.channel.send("🤖 歷史記錄重置")  
+                        return
                         
-                  await message.add_reaction('💬')
+                    await message.add_reaction('💬')
                 
-                  if self.MAX_HISTORY == 0:
+                    if self.MAX_HISTORY == 0:
                 
-                      response = await self.generate_response_with_text(cleaned_text)
-                      await self.split_and_send_messages(message, response, 1700)  
-                      return
+                        response = await self.generate_response_with_text(cleaned_text)
+                        await self.split_and_send_messages(message, response, 1700)  
+                        return
                 
-                  self.update_message_history(message.author.id, cleaned_text)  
-                  history = self.get_formatted_message_history(message.author.id)
+                    self.update_message_history(message.author.id, cleaned_text)  
+                    history = self.get_formatted_message_history(message.author.id)
                 
-                  response = await self.generate_response_with_text(history)
-                  self.update_message_history(message.author.id, response)   
-                  await self.split_and_send_messages(message, response, 1700)
+                    response = await self.generate_response_with_text(history)
+                    self.update_message_history(message.author.id, response)   
+                    await self.split_and_send_messages(message, response, 1700)
 
-  async def generate_response_with_text(self, message):
-      prompt_parts = [message]
-      print(f"Got text prompt: {message}")
+    async def generate_response_with_text(self, message):
+        prompt_parts = [message]
+        print(f"Got text prompt: {message}")
     
-      response = self.text_model.generate_content(prompt_parts)
+        response = self.text_model.generate_content(prompt_parts)
     
-      if response._error:
-          return f"❌ {response._error}"
+        if response._error:
+            return f"❌ {response._error}"
         
-      return response.text
+        return response.text
   
-  async def generate_response_with_image_and_text(self, image_data, text):
-      image_parts = [{"mime_type": "image/jpeg", "data": image_data}]  
-      prompt_parts = [image_parts[0], f"\n{text if text else 'What is this a picture of?'}"]
+    async def generate_response_with_image_and_text(self, image_data, text):
+        image_parts = [{"mime_type": "image/jpeg", "data": image_data}]  
+        prompt_parts = [image_parts[0], f"\n{text if text else 'What is this a picture of?'}"]
     
-      response = self.image_model.generate_content(prompt_parts)
+        response = self.image_model.generate_content(prompt_parts)
     
-      if response._error:
-          return f"❌ {response._error}"
+        if response._error:
+            return f"❌ {response._error}"
         
-      return response.text  
+        return response.text  
 
-  def update_message_history(self, user_id, text):
-      if user_id in self.message_history:
-          self.message_history[user_id].append(text)
-          if len(self.message_history[user_id]) > self.MAX_HISTORY:
-              self.message_history[user_id].pop(0)
-      else:
-          self.message_history[user_id] = [text]
+    def update_message_history(self, user_id, text):
+        if user_id in self.message_history:
+            self.message_history[user_id].append(text)
+            if len(self.message_history[user_id]) > self.MAX_HISTORY:
+                self.message_history[user_id].pop(0)
+        else:
+            self.message_history[user_id] = [text]
 
-  def get_formatted_message_history(self, user_id):
-      if user_id in self.message_history:
-          return "\n\n".join(self.message_history[user_id])
-      else:
-          return "No message history"  
+    def get_formatted_message_history(self, user_id):
+        if user_id in self.message_history:
+            return "\n\n".join(self.message_history[user_id])
+        else:
+            return "No message history"  
 
-  async def split_and_send_messages(self, message, text, max_length):
-      messages = [text[i:i+max_length] for i in range(0, len(text), max_length)]
+    async def split_and_send_messages(self, message, text, max_length):
+        messages = [text[i:i+max_length] for i in range(0, len(text), max_length)]
     
-      for msg in messages: 
-          await message.channel.send(msg)
+        for msg in messages: 
+            await message.channel.send(msg)
         
-  def clean_discord_message(self, input_string):
-      pattern = re.compile(r"<[^>]+>")
-      return pattern.sub("", input_string)
+    def clean_discord_message(self, input_string):
+        pattern = re.compile(r"<[^>]+>")
+        return pattern.sub("", input_string)
 
 async def setup(bot):
     await bot.add_cog(Gemini(bot))
