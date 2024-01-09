36e40b9d754639ffa56f7ee777d10ba8f6d3894b
aishukander <aishukander@gmail.com>
2024-01-09 21:19:58 +0800

    讓一個伺服器可同時擁有多個動態語音

--- cogs/DynamicVoice.py
+++ cogs/DynamicVoice.py
@@ -17,61 +17,90 @@ class DynamicVoice(Cog_Extension):
     with open(DynamicVoice_json_path,"r",encoding="utf8") as f:
         origin_channels = json.load(f)
 
-    # 定義 dv 指令用於創建母頻道
     @commands.command()
-    async def dv(self, ctx, action: str, name: str):
-        if ctx.author.guild_permissions.administrator:
-            if action.lower() == "add":
-                global origin_channels
-                global DynamicVoice_json_path
-                origin_channel = await ctx.guild.create_voice_channel(name=name) 
-                self.origin_channels[str(ctx.guild.id)] = origin_channel.id
-                await ctx.send(f"動態語音 {origin_channel.name} 已建立")
+    async def dv(self, ctx, action, name=None):
+        global origin_channels
+        global DynamicVoice_json_path
+        guild_id = str(ctx.guild.id)
 
-                # 將更新後的動態資料寫入 JSON 文件
-                with open(self.DynamicVoice_json_path,"w",encoding="utf8") as f:
-                    json.dump(self.origin_channels, f, indent=2)
-
-            elif action.lower() == "remove" and name.lower() == "true":
-                guild_id = str(ctx.guild.id) 
-                if guild_id in self.origin_channels:
-                    origin_channel_id = self.origin_channels[guild_id]
-                    origin_channel = self.bot.get_channel(origin_channel_id)
-                    await origin_channel.delete()
+        if action.lower() == "add":
+            channel = await ctx.guild.create_voice_channel(name)
         
-                    with open(self.DynamicVoice_json_path,"r",encoding="utf8") as f:
-                        json_data = json.load(f)
-                        json_data.pop(guild_id)
+            if guild_id not in self.origin_channels:
+                self.origin_channels[guild_id] = []
             
-                    with open(self.DynamicVoice_json_path,"w",encoding="utf8") as f:  
-                        json.dump(json_data, f, indent=2)
+            self.origin_channels[guild_id].append(channel.id)
+
+            self.save_data()
+            await ctx.send(f"動態語音 {channel.name} 已建立")
+
+        elif action.lower() == "remove":
+            channels = self.origin_channels.get(guild_id)
+            if not channels:
+                return await ctx.send("此伺服器未設定動態語音")
+
+            for idx, ch_id in enumerate(channels):
             
-                    await ctx.send("動態語音已刪除") 
-                else:
-                    await ctx.send("此伺服器未設定動態語音")
+                channel = self.bot.get_channel(ch_id)
+                if channel.name == name:
+                
+                    await channel.delete()
+                
+                    # 更新json数据
+                    del self.origin_channels[guild_id][idx]  
+                    if not self.origin_channels[guild_id]:
+                            self.origin_channels.pop(guild_id)
+
+                    self.save_data()
+                
+                    await ctx.send(f"動態語音 {name} 已刪除")
+                    return
+
+            await ctx.send(f"未找到 {name} 動態語音")
+        
+    def save_data(self):
+        global origin_channels
+        if not self.origin_channels:
+            json_str = "{}" 
+        else:
+            data = {}
+            formatted_data = ""
+            for guild_id, channel_ids in self.origin_channels.items():
+                formatted_data += f'\n"{guild_id}": {channel_ids},'
+
+            json_str = "{" + formatted_data[:-1] + "\n}"
+
+        with open(self.DynamicVoice_json_path,"w",encoding="utf8") as f:
+            f.write(json_str)
 
     # 監聽語音頻道變化事件
     @commands.Cog.listener() 
     async def on_voice_state_update(self, member, before, after):
+
         global voice_channel_set
-        global VoiceName
-        guild_id = member.guild.id
-        # 從 JSON 文件中獲取當前服務器的母頻道 ID
-        origin_channel_id = self.origin_channels.get(str(guild_id))
-        # 如果成員加入了一個語音頻道
-        if after.channel and after.channel.id == origin_channel_id:
-                # 創建一個新的語音頻道，名稱為成員的暱稱，類型為語音，位於原始頻道的下方
-                new_voice_channel = await after.channel.clone(name=f"{member.display_name}{VoiceName}", reason="Create new voice channel")
-                # 將成員移動到新的語音頻道
-                await member.move_to(new_voice_channel, reason="Move to new voice channel")
-                # 將新的語音頻道的ID加入到集合中，以便之後使用
-                voice_channel_set.add(new_voice_channel.id)
+
+        guild_id = str(member.guild.id)
+
+        # 获取当前服务器母频道id列表
+        origin_channels = self.origin_channels.get(guild_id)
+        if not origin_channels:
+            return
+
+        for channel_id in origin_channels:
         
-        # 如果成員離開了一個語音頻道
-        if before.channel and before.channel.id in voice_channel_set:
-                # 如果該語音頻道沒有任何成員了，也就是空的
-                if len(before.channel.members) == 0:
-                    # 刪除該語音頻道，並從集合中移除它的ID
+            # 检查是否加入母频道
+            if after.channel and after.channel.id == channel_id:
+            
+                new_channel = await after.channel.clone(name=f"{member.display_name}{VoiceName}")
+            
+                await member.move_to(new_channel)
+            
+                voice_channel_set.add(new_channel.id)
+            
+            # 检查是否离开自定义语音频道
+            elif before.channel and before.channel.id in voice_channel_set:
+          
+                if len(before.channel.members) == 0: 
                     await before.channel.delete(reason="Delete empty voice channel")
                     voice_channel_set.remove(before.channel.id)
 
