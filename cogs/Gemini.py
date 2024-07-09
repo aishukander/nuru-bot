9000bdc85165a173b9f873baa9c04de39ba101e1
aishukander <hank960924@gmail.com>
2024-07-10 07:31:27 +0800

    chore: update Gemini model to version 1.5-flash

--- cogs/Gemini.py
+++ cogs/Gemini.py
@@ -38,7 +38,7 @@ class Gemini(commands.Cog):
         {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_NONE"},
         {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_NONE"}
     ]
-    text_model = genai.GenerativeModel(model_name="gemini-1.5-pro", generation_config=text_generation_config, safety_settings=safety_settings)
+    text_model = genai.GenerativeModel(model_name="gemini-1.5-flash", generation_config=text_generation_config, safety_settings=safety_settings)
     image_model = genai.GenerativeModel(model_name="gemini-pro-vision", generation_config=image_generation_config, safety_settings=safety_settings)
 
     @commands.Cog.listener()
