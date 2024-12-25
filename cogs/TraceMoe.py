8781d209f46ff9d44e9a601d875113b50c1ce332
aishukander <aishukander@gmail.com>
2024-12-25 17:18:15 +0800

    優化圖片搜尋

--- cogs/TraceMoe.py
+++ cogs/TraceMoe.py
@@ -10,13 +10,21 @@ class TraceMoe(commands.Cog):
         self.bot = bot
         self.api_url = "https://api.trace.moe/search"
 
-    @commands.slash_command(description="找出圖片在動漫中的位置")
-    @discord.option("image", type=discord.SlashCommandOptionType.attachment, description="圖片")
-    async def trace_search(self, ctx, image: discord.Attachment):
+    @commands.slash_command(description="找出圖片在動漫中的位置(檔案/url擇一)")
+    @discord.option("image", type=discord.SlashCommandOptionType.attachment, description="圖片", required=False)
+    @discord.option("image_url", type=discord.SlashCommandOptionType.string, description="url", required=False)
+    async def trace_search(self, ctx, image: discord.Attachment = None, image_url: str = None):
         await ctx.defer()
         try:
+            if image:
+                url = image.url
+            elif image_url:
+                url = image_url
+            else:
+                await ctx.respond("請提供圖片或圖片的url")
+                return
             response = requests.get("https://api.trace.moe/search?cutBorders&url={}"
-              .format(urllib.parse.quote_plus(image.url))
+              .format(urllib.parse.quote_plus(url))
             ).json()
             result = response["result"][0]
 
@@ -29,8 +37,7 @@ class TraceMoe(commands.Cog):
                 Name = re.sub(r'\.\w{3,4}$', '', result["filename"])
 
             color = random.randint(0, 16777215)
-            embed=discord.Embed(title="來自trace.moe-api的資訊", color=color)
-            embed.add_field(name="動漫名", value=Name, inline=True)
+            embed=discord.Embed(title=Name, color=color)
             embed.add_field(name="集數", value=f"第{result['episode']}集", inline=True)
             embed.add_field(name="位置", value=Time, inline=True)
             embed.set_image(url=f"{result['image']}")
